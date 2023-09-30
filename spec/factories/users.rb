e882708f099153e5852f5c9e196ded46a777aa0e
Vermouth2004 <frodobaggines1954@gmail.com>
2023-09-30 17:20:59 +0900

    Userモデル単体テストの準備

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     first_name {'太郎'}
     last_name_kana {'ヤマダ'}
     first_name_kana {'タロウ'}
-    birth_date{Faker::Date.birthday} 
+    birth_date{'1991-01-01'} 
 
     
   end
