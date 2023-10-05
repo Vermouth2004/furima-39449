4adaeccaacdd217730db359b900919016d94b077
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-05 18:26:08 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
-
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  
+  
 
 
 end
