e05bcb502a93fd5603f1f01e5aec8f083610bda9
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-21 17:55:25 +0900

    purchasesテーブルにデータを保存するための記述②

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,11 +2,25 @@ class PurchasesController < ApplicationController
 
   def index
     @purchase = Purchase.new
-    @item = @purchase.item
-
+    @item = Item.find(params[:id])
+    
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.save
+      redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
 
+
+  
+
+
+  private
+  def purchase_params
+    params.require(:purchase).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
