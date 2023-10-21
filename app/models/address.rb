aaae3bc2208a6cc1872e14a44db6caa7fd14be63
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-21 18:30:47 +0900

    addressモデルのバリデーション及びアソシエーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,22 @@
 class Address < ApplicationRecord
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :street_address, presence: true
+  validates :phone_number, presence: true
+  
+  belongs_to :purchase
+
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  
+  validates :phone_number, format: { with: /\A\d{10, 11}\z/ }
+
+
+  
+
+
+
+
+
 end
+
