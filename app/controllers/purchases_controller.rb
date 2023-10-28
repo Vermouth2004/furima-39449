72f76a28b38d1726aff2823f258451e57c3da065
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-28 16:27:43 +0900

    データ保存のための記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -28,7 +28,7 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   
