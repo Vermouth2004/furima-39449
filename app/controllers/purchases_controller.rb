db385d90c5bc77987a655618ec92b694c9699bf6
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-28 15:32:52 +0900

    PurchaseAddressクラスのインスタンス生成及びデータ保存の記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,8 +5,21 @@ class PurchasesController < ApplicationController
     # @item = Item.find(params[:item_id])
   end
 
+
+  def new
+    @purchase_address = PurchaseAddress.new
+  end
+
+
   def create
-   
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    
   end
 
 
@@ -14,5 +27,9 @@ class PurchasesController < ApplicationController
 
 
   private
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
   
 end
