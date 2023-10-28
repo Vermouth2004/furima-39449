c5cf6c1a678746fde0e94c062da383ed41a95961
Vermouth2004 <frodobaggines1954@gmail.com>
2023-10-28 19:29:04 +0900

    PurchaseAddressクラスバリデーションの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,15 +9,14 @@ class PurchaseAddress
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :street_address
-    validates :phone_number, format: { with: /\A[0-9]{10, 11}\z/ }
-    validates :purchase_id
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
     validates :user_id
     validates :item_id
   end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 
 
